d24534625307b26115fc5d304b348dc9a1aa5ea7
Danil <silius12528@gmail.com>
2025-08-05 23:17:59 +0300

    fix test app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/slicers/store";
 
 describe("Test App", () => {
   test("include div", () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(screen.getByTestId("divApp")).toBeInTheDocument();
   });
 });
