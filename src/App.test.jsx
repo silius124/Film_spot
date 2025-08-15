004b100d3e987ab5ee6115e297e626b504984462
Danil <silius12528@gmail.com>
2025-08-15 13:43:00 +0300

    fix

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/slicers/store";
+import store from "./store/store";
 
 describe("Test App", () => {
   test("include div", () => {
