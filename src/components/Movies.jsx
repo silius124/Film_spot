7051d2651e8d5e67cda0566bf73763389e01d819
silius124 <silius125@yahoo.com>
2026-02-28 12:53:03 +0300

    style: redesign movie card in favourites

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -38,20 +38,15 @@ function Movies({ filter }) {
         favourites.map((movie) => {
           return (
             <li
-              key={movie.imdbID}
               className="card"
-              style={{ backgroundImage: `url(${movie.Poster})` }}
+              key={movie.imdbID}
               onClick={() => handleClickToMovie(movie.Title)}
             >
-              {movie.Title} был выпущен в {movie.Year}{" "}
-              <button
-                className="btn-box-red"
-                style={{ backgroundImage: 'url("./icons/favourite.svg")' }}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleClickDeleteFavourite(movie);
-                }}
-              ></button>
+              <img src={movie.Poster} alt="poster" className="poster" />
+              <div className="title">
+                <h3>{movie.Title} </h3>
+                <span>{movie.Year}</span>
+              </div>
             </li>
           );
         })}
